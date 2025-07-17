1bc0e7efc946145d5a7e0c0a05fc481d3f87f7a8
Harishna-K <harishna.gkc@gmail.com>
2025-07-17 23:53:36 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -219,28 +219,20 @@ export const education = [
 export const projects = [
   {
     id: 9,
-    title: "Trackify",
-    date: "Jun 2023 - Jul 2023",
+    title: "Eduford",
+    date: "Dec 2023 - Feb 2024",
     description:
-      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+      "A responsive educational website that simulates a university landing page. Features sections like Home, About, Courses, Campus Gallery, and Contact. Built using HTML, CSS, and JavaScript with a focus on clean UI and mobile-friendly design.",
     image:
-      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+      "C:\\Users\\Harishna\\OneDrive\\Desktop\\Portfolio\\public\\assets\\Eduford.png",
     tags: [
-      "Docker",
-      "AWS",
-      "DuckDNS",
-      "Eslint",
-      "Husky",
-      "CI/CD",
-      "React Js",
-      "MongoDb",
-      "Node Js",
-      "Express Js",
-      "Redux",
+      "HTML",
+      "CSS",
+      "Javascript"
     ],
-    category: "web app",
-    github: "https://github.com/rishavchanda/Trackify",
-    webapp: "https://trackify.duckdns.org",
+    category: "Frontend Web Project",
+    github: "https://github.com/Harishna-K/Eduford_website",
+    website: "https://harishna-k.github.io/Eduford_website/",
   },
   {
     id: 0,
